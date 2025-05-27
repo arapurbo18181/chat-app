962bf374850a9fbec9bb72758621f228c3cbaeac
MD Abdul Zabber Islam Apurbo <md.abdul.zabber.islam.apurbo@g.bracu.ac.bd>
2025-05-27 22:04:28 +0600

    signup functionally added and api endpoint is working.

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,8 +1,56 @@
-const signup = (req, res) => {
-  res.status(201).json({
-    statusCode: 201,
-    message: "Signup route",
-  });
+const { generateToken } = require("../lib/utils");
+const User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+
+// ! Signup
+const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    // ! initial check
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // ! checking password length
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    // ! checking if email already exists
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    // ! encrypting password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    // ! generating JWT token
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      return res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      return res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller");
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 const login = (req, res) => {
--- server/src/index.js
+++ server/src/index.js
@@ -2,11 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const authRoutes = require("./routes/auth.route");
 const config = require("./config/config");
-const { connectDb } = require("../connection");
+const { connectDb } = require("./connection");
 
 const app = express();
 const port = config.port;
 const dbUrl = config.db.baseUrl;
+
+// ! Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ! Routes
 app.use("/api/auth", authRoutes);
