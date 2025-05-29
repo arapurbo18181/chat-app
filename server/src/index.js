8a6777e8623260afb0c8e1b011616b18e69bef86
MD Abdul Zabber Islam Apurbo <md.abdul.zabber.islam.apurbo@g.bracu.ac.bd>
2025-05-30 00:04:28 +0600

    chat page sidebar implemented

--- server/src/index.js
+++ server/src/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 // ! Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 // ! Port Connection
 app.listen(port, () => {
